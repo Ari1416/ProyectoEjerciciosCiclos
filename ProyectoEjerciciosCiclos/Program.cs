a601225533c20fa813f4729b269aa3e555d7a867
Ari <147622495+Ari1416@users.noreply.github.com>
2024-01-29 16:02:59 -0400

    Aunque no lo crea, esto me ha tomado 1 hora, jejeje
    Ejercicio Listoo!!!!!

--- ProyectoEjerciciosCiclos/Program.cs
+++ ProyectoEjerciciosCiclos/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hell0!!!");
+﻿// Leer un número entero y mostrar todos los pares comprendidos entre 1 y el número leído.
+
+Console.Write("Digite el numero por favor: ");
+int Numeropar = Convert.ToInt32(Console.ReadLine());
+
+int Contador = 0;
+
+while (Contador <= Numeropar)
+{
+    Console.WriteLine(Contador);
+    Contador+=2;
+}
+
